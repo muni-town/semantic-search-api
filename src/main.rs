1e3bf76fbf2174934809a7bfbcc0b9acf82e3474
Zicklag <zicklag@katharostech.com>
2025-07-23 20:55:50 -0500

    feat: add index endpoint and uuid handling.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use std::sync::Arc;
 
 use poem::{
-    Body, EndpointExt, Route, Server, handler, listener::TcpListener, middleware::AddData, post,
-    web::Data,
+    Body, EndpointExt, Route, Server,
+    error::ResponseError,
+    get, handler,
+    http::StatusCode,
+    listener::TcpListener,
+    middleware::AddData,
+    post,
+    web::{Data, Json, Path},
 };
 
 use clap::Parser;
-use semantic_search_api::Engine;
+use qdrant_client::{Payload, qdrant::SearchPointsBuilder};
+use semantic_search_api::{Engine, Item};
+use serde_json::Value;
 use tokio::sync::Mutex;
+use uuid::{Uuid, uuid};
+
+const UUID_NAMESPACE: Uuid = uuid!("da0ac261-2851-4934-a405-a1df024749cb");
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -27,14 +38,70 @@ struct Args {
 
 type EngineState<'a> = &'a Arc<Mutex<Engine>>;
 
+type Result<T, E = Error> = std::result::Result<T, E>;
+#[derive(Debug)]
+struct Error(anyhow::Error);
+impl From<anyhow::Error> for Error {
+    fn from(value: anyhow::Error) -> Self {
+        Self(value)
+    }
+}
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+impl std::error::Error for Error {}
+impl ResponseError for Error {
+    fn status(&self) -> poem::http::StatusCode {
+        StatusCode::INTERNAL_SERVER_ERROR
+    }
+}
+
 #[handler]
-async fn embed_endpoint(body: Body, engine: Data<EngineState<'_>>) -> String {
+async fn post_embed(body: Body, engine: Data<EngineState<'_>>) -> Result<Json<Value>> {
     let mut engine = engine.lock().await;
     let text = body.into_string().await.unwrap();
+    let embed = engine.model.embed(vec![text], None)?.remove(0);
+    Ok(Json(embed.into()))
+}
 
-    let embed = engine.model.embed(vec![text], None);
+#[handler]
+async fn get_uuid(Path(id): Path<String>) -> String {
+    Uuid::new_v5(&UUID_NAMESPACE, id.as_bytes()).to_string()
+}
 
-    format!("hello: {:?}", embed)
+#[handler]
+async fn post_index(
+    Path(id): Path<String>,
+    body: Body,
+    engine: Data<EngineState<'_>>,
+) -> Result<Json<Value>> {
+    let uuid = Uuid::new_v5(&UUID_NAMESPACE, id.as_bytes());
+    let mut engine = engine.lock().await;
+    let mut payload = Payload::new();
+
+    payload.insert("id", id);
+    engine
+        .index([Item {
+            id: uuid,
+            text: body.into_string().await.map_err(anyhow::Error::from)?,
+            payload,
+        }])
+        .await?;
+    let json = serde_json::json!({
+        "uuid": uuid,
+    });
+    Ok(Json(json))
+}
+
+#[handler]
+async fn post_search(body: Body, engine: Data<EngineState<'_>>) -> Result<Json<Value>> {
+    let mut engine = engine.lock().await;
+    let mut payload = Payload::new();
+
+    let json = todo!();
+    Ok(Json(json))
 }
 
 #[tokio::main]
@@ -43,7 +110,10 @@ async fn main() -> anyhow::Result<()> {
     let engine = Engine::start(&args.qdrant_url, &args.qdrant_collection).await?;
 
     let app = Route::new()
-        .at("/embed", post(embed_endpoint))
+        .at("/embed", post(post_embed))
+        .at("/index/:id", post(post_index))
+        .at("/uuid/:id", get(get_uuid))
+        .at("/search", post(post_search))
         .with(AddData::new(Arc::new(Mutex::new(engine))));
 
     Server::new(TcpListener::bind(format!("0.0.0.0:{}", args.listen_port)))
